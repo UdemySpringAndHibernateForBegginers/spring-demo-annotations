37092410f30436758c7ec8393d8b841f8b54e6ec
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-05-05 22:04:57 +0200

    Added logic to RandomFortuneService

--- src/main/java/com/luv2code/springdemo/service/RandomFortuneService.java
+++ src/main/java/com/luv2code/springdemo/service/RandomFortuneService.java
@@ -2,11 +2,18 @@ package com.luv2code.springdemo.service;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService {
 
+    private String[] data = {"Message 1 from RANDOM Fortune Service!", "Message 2 from RANDOM Fortune Service!", "Message 3 from RANDOM Fortune Service!"};
+
+    private Random random = new Random();
+
     @Override
     public String getFortune() {
-        return "Message from RANDOM Fortune Service!";
+        int index = random.nextInt(data.length);
+        return data[index];
     }
 }
